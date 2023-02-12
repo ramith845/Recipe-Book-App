d459fb61f8be0a40f39774ed8dd7c034615c7d72
Ramith <ramith845@gmail.com>
2023-02-12 18:35:00 +0530

    Shared files tracked

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, OnInit, HostBinding, HostListener , Renderer2 } from "@angular/core";
+
+@Directive({
+    selector: '[app-dropdown]'
+})
+
+export class DropdownDirective implements OnInit {
+    @HostBinding('attr.data-bs-toggle') toggle: string;
+    @HostBinding('class.dropdown-toggle') dropDown: boolean;
+
+    constructor(private ele: ElementRef, private rend: Renderer2) { }
+
+    ngOnInit(): void {
+        this.ele.nativeElement.ariaExpanded = 'false';
+        // this.rend.setAttribute(this.ele.nativeElement, 'data-bs-toggle', 'dropdown')
+        // this.rend.addClass(this.ele.nativeElement, 'dropdown-toggle')
+        this.toggle = 'dropdown'
+        this.dropDown = true
+        // console.log(this.ele);
+        
+    }
+
+    // @HostListener('click')
+}
